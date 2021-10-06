3177dae44a26896c02e7070f7bd4a519071eab94
AndrewCarrierMurillo <Andrew.S.Murillo@gmail.com>
2021-10-06 13:04:17 -0700

    Created function to return computer choice and function to play a single round of game.

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+// function returns computers turn based on if variable equals 0, 1, or 2.
+function computerPlay() {
+    let move = Math.floor(Math.random() * 3);
+
+    if (move === 0) {
+        return "rock";
+    } else if (move === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// function takes in user input, lowercases it to normalize string, and compares to computer choice, returning result of single game.
+function round(computerSelection, playerSelection) {
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection;
+
+    if (player === "rock" && computer === "rock") {
+        return "Tie. Rock and Rock.";
+    } else if (player === "rock" && computer === "paper") {
+        return "You lose. Paper beats Rock.";
+    } else if (player === "rock" && computer === "scissors") {
+        return "You win. Rock beats Scissors.";
+    } else if (player === "paper" && computer === "paper") {
+        return "Tie. Paper and Paper.";
+    } else if (player === "paper" && computer === "rock") {
+        return "You win. Paper beats Rock.";
+    } else if (player === "paper" && computer === "scissors") {
+        return "You lose. Scissors beats Paper.";
+    } else if (player === "scissors" && computer === "scissors") {
+        return "Tie. Scissors and Scissors.";
+    } else if (player === "scissors" && computer === "rock") {
+        return "You lose. Rock beats Scissors.";
+    } else if (player === "scissors" && computer === "paper") {
+        return "You win. Scissors beats Paper.";
+    } else {
+        return "Please choose either rock, paper, or scissors.";
+    }
+
+}
+
+playerSelection = "rock";
+computerSelection = computerPlay();
